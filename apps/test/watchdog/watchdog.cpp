362ba883e4c8f8dddfcbe64aeff69ab309a0892d
Peter Vizi <peter.vizi@gmail.com>
2008-12-20 19:48:32 -0500

    Blink the onboard LED 3 times and go to sleep.

--- apps/test/watchdog/watchdog.cpp
+++ apps/test/watchdog/watchdog.cpp
@@ -1,5 +1,24 @@
+#include <avr/sleep.h>
+
+int ledPin = 13;
+
 void setup() {
+    pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
+    digitalWrite(ledPin, HIGH);
+    delay(1000);
+    digitalWrite(ledPin, LOW);
+    delay(1000);
+    digitalWrite(ledPin, HIGH);
+    delay(1000);
+    digitalWrite(ledPin, LOW);
+    delay(1000);
+    digitalWrite(ledPin, HIGH);
+    delay(1000);
+    digitalWrite(ledPin, LOW);
+
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+    sleep_mode();
 }
